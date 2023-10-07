b05fabdba2d0adc92a9b0f305dff63119e6e6ce5
EsraaMuhammed98 <esraamohamed651@gmail.com>
2023-10-07 21:25:59 +0300

    updates

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
   return <>
-     <div className="footer">
+     <div className="footer fixed-bottom ">
       <p className='bg-dark p-3 mb-0 text-center text-main'>&copy; Copy Right 2023 </p>
      </div>
   </>
