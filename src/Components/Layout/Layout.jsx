f1a11b750a1736d2353d706a7fa98e7ddad5c777
EsraaMuhammed98 <esraamohamed651@gmail.com>
2023-10-07 21:19:01 +0300

    updates

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -8,13 +8,32 @@ import { useState ,useContext} from 'react';
 import { userContext } from '../../Context/userContext';
 import { useEffect } from 'react';
 import NotFound from '../NotFound/NotFound';
+import Footer from '../Footer/Footer';
 export default function Layout() {
   let {userToken,setUserToken}= useContext(userContext)
+  
+  let scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    setShowButton(false);
+  };
+  const [showButton, setShowButton] = useState(false);
 
+  const handleScroll = () => {
+    if (window.pageYOffset > 300) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+  
   useEffect(()=>{
     if(localStorage.getItem('token') !== null){
-            setUserToken(localStorage.getItem('token'))
-       }
+      setUserToken(localStorage.getItem('token'))
+ }
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+
+   
   },[])
    return <>
   <Navbar/>
@@ -22,6 +41,8 @@ export default function Layout() {
   <div className={!<NotFound/>? "container" :''}>
   <Outlet></Outlet>
 
+{showButton &&<button onClick={scrollToTop} className='up btn btn-outline-success p-2 rounded-circle'>Up</button>}
   </div>
+  <Footer/>
   </>
 }
